92a8a483c7f47de9863c3d46025afcb2ca910e2f
ssnyta <94050114+ssnyta@users.noreply.github.com>
2024-11-06 11:56:46 +0100

    added files

---
+++ factory_method/factory/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace factory
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Creator []animals = new Creator[10]; // pole o n prvcích, momentálně 10-ti
+            for (int i = 0; i < animals.Length; i++){
+                Console.WriteLine("Rasa:");
+               string rasa =  Console.ReadLine();
+               Console.WriteLine("Druh:");
+               string druh = Console.ReadLine();
+               Console.WriteLine("Věk:");
+               int vek = Int32.Parse(Console.ReadLine());
+               Console.WriteLine("Zvuk:");
+               string zvuk = Console.ReadLine();
+               animals[i] = new Animal(rasa, druh, vek, zvuk); //Zapsání instance do pole zvířat
+               Console.Clear();
+            }
+            foreach (Creator animal in animals)
+            {
+                Animal zvire = (Animal)animal.FactoryMethod(); // (Animal) vrací instanci v Animal typu
+                Console.WriteLine($"Zvíře: {zvire.race}, Druh: {zvire.type}, Věk: {zvire.age} let, Zvuk: {zvire.noise}");
+                
+            }
+        }
+    }
+}       
---
+++ factory_method/factory/factory.cs
@@ -0,0 +1,30 @@
+﻿using System.Configuration.Assemblies;
+
+namespace factory
+{
+    abstract class Creator
+    {
+        public abstract Animal FactoryMethod(); // Tahhle metoda na bude vyprdávat instance pro třídu Animal
+    }
+    class Animal : Creator{
+        public override Animal FactoryMethod()
+        {
+            return new Animal(this.race,this.type,this.age, this.noise);
+        }
+        public string race;
+        public string type;
+        public int age;
+        public string noise;
+        public Animal(string race, string type, int age, string noise)
+        {
+            this.race = race;
+            this.type = type;
+            this.age = age;
+            this.noise =  noise;
+        }
+        public string MakeSomeNoise (){
+           return "${this.noise}";
+        }
+    }
+}
+
