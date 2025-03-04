0a369f2fd3f16accc8305a85e9d7983efbecb360
ssnyta <94050114+ssnyta@users.noreply.github.com>
2025-03-04 09:57:05 +0100

    bnmbkj

--- factory_method/singleton/obj/Debug/net8.0/singleton.AssemblyInfo.cs
+++ factory_method/singleton/obj/Debug/net8.0/singleton.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("singleton")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+761124d812654ebeced2fefde311ac77d2427f80")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+768b805e281fe197bbb6d50927fd8825f8651d42")]
 [assembly: System.Reflection.AssemblyProductAttribute("singleton")]
 [assembly: System.Reflection.AssemblyTitleAttribute("singleton")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
